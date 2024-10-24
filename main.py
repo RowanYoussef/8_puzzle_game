ee6dcae71f36ebcd28473dde3d1a252220c6774d
RowanYoussef <150041546+RowanYoussef@users.noreply.github.com>
2024-10-24 22:24:09 +0300

    edit

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ print(states_object.check_goal(12345678))
 print(states_object.get_direction(52431876,452031876))
 print(states_object.get_next_state(52431876))
 
-bfs = algorithms.bfs.BFS(120345678)
+bfs = algorithms.bfs.BFS(867254301)
 print(bfs.BFS())
 print(bfs.get_path())
 print(bfs.get_nodes_expanded())
+print(len(bfs.get_nodes_expanded()))
 print(bfs.get_search_level())
-
+print(len(bfs.get_path()))
